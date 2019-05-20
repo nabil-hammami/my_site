f04cb4104d62e71d95c9c91876ac13358614e898
Nabil <nabilh17@pm.me>
2019-05-20 17:32:52 +0200

    Test of logBook button finished

---
+++ JS/menu.js
@@ -0,0 +1,8 @@
+// Objective is to have a fully generated menu by script
+// to finally get info from a database
+// i.e.
+// | LABEL |     URL     |   TARGET  |
+// | Home  | ./home.html | mainframe |
+// | ...   |     ...     |    ...    |
+
+
